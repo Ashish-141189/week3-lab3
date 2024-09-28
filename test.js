c4ed1e6d94ffbffb6f289fd7fba3833073a5fe9d
Ashish-141189 <ashishmohan141189@gmail.com>
2024-09-28 14:50:08 +0300

    Activity 3

---
+++ test.js
@@ -0,0 +1,7 @@
+const customFunctions = require('./custom-functions');
+
+console.log(customFunctions.cube(33)); // Test the cube function
+console.log(customFunctions.fullName('Ashish', 'Mohan')); // Test the fullName function
+console.log(customFunctions.power(3, 3)); // Test the power function
+console.log(customFunctions.sumCubes([2, 3, 4])); // Test the sumCubes function
+
